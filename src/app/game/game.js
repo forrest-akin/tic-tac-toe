6a40dc67186497b2b15ff248506d51ff19665e59
Forrest Akin <forrest.akin@gmail.com>
2018-09-04 16:41:21 -0700

    finished game logic

--- src/app/game/game.js
+++ src/app/game/game.js
@@ -1,48 +1,72 @@
-import { getElementById, insertText } from '../dom/dom.js'
+import ResetButton from '../components/resetButton/resetButton.js'
 import {
-  getState, isComputerTurn, isFirstMove, isGameOver, togglePiece, togglePlayer, updateBoard
+  appendChild, applyStyles, clickElement, getElementById,
+} from '../dom/dom.js'
+import {
+  gameOver, getState, initState, isComputerTurn, isFirstMove,
+  togglePiece, togglePlayer, updateBoard,
 } from '../state/state.js'
+import { getRandomItem } from '../utils/utils.js'
 import minimax from './minimax.js'
+import {
+  getCell, getEmptyCells, getWinningIndexes, isTie, isWinPossible
+} from './utils.js'
 
-export function makeMove(cell) {
-  if (isGameOver()) return
-
-  const { currentPlayer, piece } = getState()
-
-  insertText(cell, piece)
-  updateBoard(getBoardIndexes(cell), piece)
+export function makeMove (indexes, piece) {
   togglePiece(piece)
-  togglePlayer(currentPlayer)
+  togglePlayer(getState('currentPlayer'))
+  evaluateGameState(updateBoard(indexes, piece))
   play()
 }
 
-export function play() {
+export function play () {
   isComputerTurn() && makeComputerMove()
+}
+
+export function resetGame () {
+  initState()
+  play()
+}
+
+const evaluateGameState = (board) => {
+  const emptyCells = getEmptyCells(board)
+  if (!isWinPossible(emptyCells)) return
+  
+  const win = getWinningIndexes(board)
+  if (win || isTie(emptyCells)) endGame(win)
+}
+
+const endGame = (win) => {
+  gameOver(win)
+  styleWinners(win)
+  showResetButton()
 }
 
 const makeComputerMove = () => {
   const { indexes } = getBestMove(cloneBoard())
-  clickCell(indexes)
+  if (indexes) clickCell(indexes)
 }
 
-const getBestMove = board =>
-  isFirstMove(board)
-  ? getFirstMove()
-  : minimax(getState('piece'), board)
+const styleWinners = (win = []) =>
+  win.forEach(indexes => applyStyles(getCell(indexes), winStyles))
+
+const winStyles = { 'background-color': 'yellow' }
+
+const showResetButton = () => appendChild(getGame(), ResetButton())
+
+const getBestMove = (board) => {
+  return isFirstMove()
+    ? getFirstMove()
+    : minimax(getState('piece'), board)
+}
 
 const cloneBoard = () => getState('board').map(row => [...row])
 
-const clickCell = indexes => getCell(indexes).click()
+const clickCell = (indexes) => clickElement(getCell(indexes))
 
 const getFirstMove = () => ({ indexes: getRandomItem(firstMoves) })
 
-const getRandomItem = arr => arr[Math.floor(Math.random() * arr.length)]
-
-const getCell = ([ i, j ]) => getElementById(`${i}-${j}`)
-
-const getBoardIndexes = cell => parseId(cell.getAttribute('id'))
-
-const parseId = id => id.split('-')
+const getGame = () => getElementById('game')
 
 // best first move is any corner
 // https://puzzling.stackexchange.com/questions/30/what-is-the-optimal-first-move-in-tic-tac-toe
