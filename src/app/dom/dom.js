d6c1e99669dd83c1d97c82e9f98385119031abf6
Forrest Akin <forrest.akin@gmail.com>
2018-09-04 16:40:04 -0700

    new dom utils

--- src/app/dom/dom.js
+++ src/app/dom/dom.js
@@ -1,26 +1,13 @@
-export function mount (element, root = getRoot()) {
-  root.parentNode.replaceChild(element, root)
-}
-
-export function getRoot (id = 'root') {
-  return getElementById(id)
-}
-
-export function getElementById (id) {
-  return document.getElementById(id)
-}
-
-export function insertText (element, string = '') {
-  element.appendChild(text(string))
+export function addEventListener (
+  element, event, { handler, options }
+) {
+  element.addEventListener(event, handler, options)
   return element
 }
 
-export function text (string = '') {
-  return document.createTextNode(string)
-}
-
-export function createElement (tag = 'div') {
-  return document.createElement(tag)
+export function appendChild (element, child) {
+  element.appendChild(child)
+  return element
 }
 
 export function applyStyles (element, styles = {}) {
@@ -28,19 +15,42 @@ export function applyStyles (element, styles = {}) {
   return element
 }
 
-export function addEventListener (
-  element, event, { handler, options = {} }
-) {
-  element.addEventListener(event, handler, options)
-  return element
+export function clickElement (element) {
+  element.click()
 }
 
+export function createElement (tag = 'div') {
+  return document.createElement(tag)
+}
+
+export function getElementById (id) {
+  return document.getElementById(id)
+}
+
+export function getRoot (id = 'root') {
+  return getElementById(id)
+}
+
+export function insertText (element, string = '') {
+  return appendChild(element, text(string))
+}
+
+export function mount (element, root = getRoot()) {
+  root.parentNode.replaceChild(element, root)
+}
+
+export function removeElement (el) {
+  el.parentNode.removeChild(el)
+}
+
+export function removeInnerContent (el) {
+  el.innerHTML = ''
+}
 export function setAttribute (element, attribute, value) {
   element.setAttribute(attribute, value)
   return element
 }
 
-export function appendChild (element, child) {
-  element.appendChild(child)
-  return element
+export function text (string = '') {
+  return document.createTextNode(string)
 }
