eaecc3e591cf5c8784067ef38cdc393fea3a76de
Forrest Akin <forrest.akin@gmail.com>
2018-09-04 16:40:54 -0700

    moved DOMy stuff in makeMove to Cell

--- src/app/components/board/row/cell/cell.js
+++ src/app/components/board/row/cell/cell.js
@@ -1,6 +1,8 @@
-import { makeMove } from '../../../../game/game.js';
+import { insertText } from '../../../../dom/dom.js'
+import { makeMove } from '../../../../game/game.js'
+import { formatCellId } from '../../../../game/utils.js'
+import { getState, isGameOver } from '../../../../state/state.js'
 import createComponent from '../../../createComponent.js'
-import { formatCellId } from './utils.js'
 
 export default function Cell ({ cellIdx, rowIdx }) {
   return createComponent({
@@ -10,21 +12,29 @@ export default function Cell ({ cellIdx, rowIdx }) {
   })
 }
 
-const onClick = ({ target: cell }) => makeMove(cell)
-
-const events = {
-  click: {
-    handler: onClick,
-    options: { once: true },
-  }
+export const styles = {
+  'background-color': 'ghostwhite',
+  'font-size':        '100px',
+  height:             '180px',
+  'line-height':      '180px',
+  'text-align':       'center', 
+  'vertical-align':   'middle',
+  width:              '180px',
 }
 
-const styles = {
-  border:           'solid',
-  'font-size':      '100px',
-  height:           '300px',
-  'line-height':    '300px',
-  'text-align':     'center', 
-  'vertical-align': 'middle',
-  width:            '300px',
+const onClick = ({ target: cell }) => {
+  if (isGameOver() || !isCellEmpty(cell)) return
+  const piece = getState('piece')
+  insertText(cell, piece)
+  makeMove(getBoardIndexes(cell), piece)
+}
+
+const isCellEmpty = (cell) => cell.innerHTML === ''
+
+const getBoardIndexes = (cell) => parseId(cell.getAttribute('id'))
+
+const parseId = (id) => id.split('-')
+
+const events = {
+  click: { handler: onClick }
 }
