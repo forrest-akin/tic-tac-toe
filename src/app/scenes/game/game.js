7cb698c6e6ba0b064589206948aed2e6f01968bc
Forrest Akin <forrest.akin@gmail.com>
2018-09-04 16:41:56 -0700

    add Game scene

---
+++ src/app/scenes/game/game.js
@@ -0,0 +1,21 @@
+import createComponent from '../../components/createComponent.js'
+import Logo from '../../components/logo/logo.js'
+import Board from '../../components/board/board.js'
+
+export default function Game ({ rows }) {
+  return createComponent({
+    attributes: { id: 'game' },
+    children: [
+      Logo(),
+      Board({ rows })
+    ],
+    styles,
+  })
+}
+
+const styles = {
+  height:      '900px',
+  margin:      'auto',
+  'max-width': '600px',
+  width:       '600px',
+}
