3ef395009b2be34764e2629d5970b52e861f40b4
Forrest Akin <forrest.akin@gmail.com>
2018-09-04 16:44:17 -0700

    finalized state module

--- src/app/state/state.js
+++ src/app/state/state.js
@@ -1,44 +1,5 @@
-export function updateBoard (indexes, piece) {
-  const board = nestedUpdate(getState('board'), indexes, piece)
-  setState({ board, isFirstMove: false })
-  return board
-}
-
-export function isFirstMove () {
-  return getState('isFirstMove')
-}
-
-export function isGameOver () {
-  return getState('isGameOver')
-}
-
-export function isComputerTurn () {
-  return getState('currentPlayer') === COMPUTER
-}
-
-export function getState (key) {
-  return key ? state[key] : state
-}
-
-export function togglePiece (prevPiece) {
-  const piece = getOppositePiece(prevPiece)
-  setState({ piece })
-  return piece
-}
-
-export function getOppositePiece (piece) {
-  return piece === 'X' ? 'O' : 'X'
-}
-
-export function togglePlayer (player) {
-  const currentPlayer = getOppositePlayer(player)
-  setState({ currentPlayer })
-  return currentPlayer
-}
-
-export function getOppositePlayer (player) {
-  return player === COMPUTER ? HUMAN : COMPUTER
-}
+import { COMPUTER, getOppositePiece, getOppositePlayer } from '../game/utils.js'
+import { nestedUpdate } from '../utils/utils.js'
 
 export function initState () {
   state = {
@@ -52,23 +13,50 @@ export function initState () {
   return state
 }
 
-const nestedUpdate = (arr, [idx, ...indexes], item) =>
-  idx === undefined
-  ? item
-  : updateItem(arr, idx, nestedUpdate(arr[idx], indexes, item))
+export function isComputerTurn () {
+  return getState('currentPlayer') === COMPUTER
+}
 
-const updateItem = (arr, idx, item) => {
-  const copy = [...arr]
-  copy.splice(idx, 1, item)
-  return copy
+export function isFirstMove () {
+  return getState('isFirstMove')
+}
+
+export function isGameOver () {
+  return getState('isGameOver')
+}
+
+export function gameOver (win) {
+  setState({ isGameOver: true, win })
+}
+
+export function getState (key) {
+  return key ? state[key] : state
+}
+
+export function togglePiece (prevPiece) {
+  const piece = getOppositePiece(prevPiece)
+  setState({ piece })
+  return piece
+}
+
+export function togglePlayer (player) {
+  const currentPlayer = getOppositePlayer(player)
+  setState({ currentPlayer })
+  return currentPlayer
+}
+
+export function updateBoard (indexes, piece) {
+  const board = nestedUpdate(getState('board'), indexes, piece)
+  setState({ board, isFirstMove: false })
+  return board
 }
 
 const setState = (change) => Object.assign(state, change)
-const initPlayer = () => Math.round(Math.random())
-const initBoard = () => initRow().map(() => initRow())
-const initRow = () => Array(3).fill(null)
 
-const HUMAN = 0
-const COMPUTER = 1
+const initPlayer = () => Math.round(Math.random())
+
+const initBoard = () => initRow().map(initRow)
+
+const initRow = () => Array(3).fill(null)
 
 let state
