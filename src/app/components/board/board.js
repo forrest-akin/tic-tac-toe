8d3ac4eae431b08e5c229b2c9404a53191d2753d
Forrest Akin <forrest.akin@gmail.com>
2018-09-04 17:15:34 -0700

    obj key-val spacing

--- src/app/components/board/board.js
+++ src/app/components/board/board.js
@@ -14,8 +14,8 @@ const mapRows = (cells, rowIdx) => Row({ cells, rowIdx })
 const attributes = { id: 'board' }
 
 const styles = {
-  height:           '600px',
-  margin:           'auto',
-  'max-width':      '600px',
-  width:            '600px',
+  height:      '600px',
+  margin:      'auto',
+  'max-width': '600px',
+  width:       '600px',
 }
