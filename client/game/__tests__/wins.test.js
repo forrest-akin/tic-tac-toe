e61bfc72e6e913cce55b7e4c20b3ea4e03c53047
Forrest Akin <forrest.akin@gmail.com>
2018-09-06 13:52:57 -0700

    add game/wins tests

---
+++ client/game/__tests__/wins.test.js
@@ -0,0 +1,50 @@
+import createWins from '../wins'
+
+describe('createWins', () => {
+  it('should create an array of arrays of winning index combinations', () => {
+    expect(createWins()).toEqual(wins)
+  })
+})
+
+const wins = [
+  [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ],
+  [
+    [1, 0],
+    [1, 1],
+    [1, 2],
+  ],
+  [
+    [2, 0],
+    [2, 1],
+    [2, 2],
+  ],
+  [
+    [0, 0],
+    [1, 0],
+    [2, 0],
+  ],
+  [
+    [0, 1],
+    [1, 1],
+    [2, 1],
+  ],
+  [
+    [0, 2],
+    [1, 2],
+    [2, 2],
+  ],
+  [
+    [0, 0],
+    [1, 1],
+    [2, 2],
+  ],
+  [
+    [2, 0],
+    [1, 1],
+    [0, 2],
+  ],
+]
